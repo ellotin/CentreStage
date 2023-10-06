031492c1165e24e44eb2ac13007a3e1eeb49d6a7
jl-23929 <james680384@brisbanegrammar.com>
2023-10-06 19:40:22 +1000

    Added Robot Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import java.util.List;
+
+public class Robot {
+   public DcMotor leftSlide;
+   private List <DcMotor> Slides;
+
+   leftSlide = hardwareMap.get(DcMotor.class, "leftSlide");
+   rightSlide = hardwareMap.get(DcMotor.class, "rightslide";
+
+}
